30399ecd2691e1c2ce8a99f712489a50b4aa0e67
DieguiinG <difejacyntho@hotmail.com>
2023-04-24 17:28:59 -0300

    Banco de dados configurado

---
+++ src/main/java/com/diego/ProgramaRh/ProgramaRhApplication.java
@@ -0,0 +1,13 @@
+package com.diego.ProgramaRh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/ProgramaRh/ProgramaRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.ProgramaRh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
