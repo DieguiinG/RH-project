49e80f4e34a541f2c8c206b0be958dbade6c6035
DieguiinG <difejacyntho@hotmail.com>
2023-04-26 17:03:40 -0300

    Listar Vagas

--- src/main/java/com/diego/ProgramaRh/Controllers/VagaController.java
+++ src/main/java/com/diego/ProgramaRh/Controllers/VagaController.java
@@ -6,10 +6,12 @@ import com.diego.ProgramaRh.repository.VagaRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -37,5 +39,14 @@ public class VagaController {
         vr.save(vaga);
         attributes.addFlashAttribute("mensagem", "Vaga cadastrada com sucesso!");
         return "redirect:/cadastrarVaga";
+    }
+    //--------------------------------- Listar Vaga ------------------------//
+
+    @RequestMapping("/vagas")
+    public ModelAndView listaVagas(){
+        ModelAndView mv = new ModelAndView("/vaga/listaVaga");
+        Iterable<Vaga> vagas = vr.findAll();
+        mv.addObject("vagas",vagas);
+        return mv;
     }
 }
